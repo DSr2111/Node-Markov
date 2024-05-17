53f1d0cd0be717444c1c72339910e72a06ee8211
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-05-16 21:33:57 -0400

    makeText comman-line

--- makeText.js
+++ makeText.js
@@ -7,3 +7,14 @@ function generateText(text) {
   let mm = new markov.MarkovMachine(text);
   console.log(mm.makeText());
 }
+
+function makeText(path) {
+  fs.readFile(path, "utf8", function cb(err, data) {
+    if (err) {
+      console.error(`Cannot read file: ${path}: ${err}`);
+      process.exit(1);
+    } else {
+      generateText(data);
+    }
+  });
+}
