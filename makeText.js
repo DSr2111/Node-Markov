f18b9fb4a00202e7da79c236f6b10ec769ba61e9
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-05-16 21:30:12 -0400

    generateText func

--- makeText.js
+++ makeText.js
@@ -2,3 +2,8 @@ const fs = require("fs");
 const markov = require("./markov");
 const axios = require("axios");
 const process = require("process");
+
+function generateText(text) {
+  let mm = new markov.MarkovMachine(text);
+  console.log(mm.makeText());
+}
